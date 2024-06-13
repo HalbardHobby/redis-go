a4e2659dea84c5201ac7c03ea49c5d92f36f4b8d
Alejandro Espinosa <5396971+HalbardHobby@users.noreply.github.com>
2024-06-13 02:34:53 -0500

    ✨ Added response to ping

--- app/server.go
+++ app/server.go
@@ -12,9 +12,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	buff := make([]byte, 128)
+	conn.Read(buff)
+
+	_, err = conn.Write([]byte(""))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
